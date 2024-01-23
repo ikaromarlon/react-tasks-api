66a591abb2fd747f1cb3c4708a8c052abe14ab2f
Icaro Silva <ikaromarlon@gmail.com>
2024-01-23 17:54:10 +0100

    feat: add TaskForm component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import TaskForm from "./components/TaskForm";
+
 function App() {
   return (
     <div className="bg-zinc-900 h-screen text-white flex items-center justify-center">
       <div className="bg-gray-950 p-4 w-2/5">
         <h1 className="text-3xl font-bold text-center block my-2">Tasks</h1>
+        <TaskForm />
       </div>
     </div>
   );
---
+++ src/components/TaskForm.tsx
@@ -0,0 +1,60 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+
+function TaskForm() {
+
+  const [task, setTask] = useState({
+    title: '',
+    description: '',
+    done: false
+  })
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setTask({ ...task, [e.target.name]: e.target.value })
+  }
+
+  const handleChangeCbx = (e: ChangeEvent<HTMLInputElement>) => {
+    setTask({ ...task, [e.target.name]: e.target.checked })
+  }
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log(task)
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="title"
+          className="border-2 border-gray-700 p-2 rounded-lg bg-zinc-800 block w-full my-2"
+          placeholder="Task title"
+          onChange={handleChange}
+          value={task.title}
+        />
+        <textarea
+          name="description"
+          rows={3}
+          className="border-2 border-gray-700 p-2 rounded-lg bg-zinc-800 block w-full my-2"
+          placeholder="Description"
+          onChange={handleChange}
+          value={task.description}
+        ></textarea>
+        <label htmlFor="inputDone" className="inline-flex items-center gap-x-2">
+          <input
+            type="checkbox"
+            name="done"
+            id="inputDone"
+            className="h-5 w-5 text-indigo-600"
+            checked={task.done}
+            onChange={handleChangeCbx}
+          />
+          <span>done</span>
+        </label>
+        <button className="bg-indigo-500 px-3 block py-2 w-full">Save</button>
+      </form>
+    </div>
+  );
+}
+
+export default TaskForm;
