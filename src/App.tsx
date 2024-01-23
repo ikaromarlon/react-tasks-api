6172711de70c5874976d638f24a1ef7adcc4cfa7
Icaro Silva <ikaromarlon@gmail.com>
2024-01-23 17:51:59 +0100

    feat: add app container

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className="bg-zinc-900 h-screen text-white flex items-center justify-center">
+      <div className="bg-gray-950 p-4 w-2/5">
+        <h1 className="text-3xl font-bold text-center block my-2">Tasks</h1>
+      </div>
     </div>
   );
 }
